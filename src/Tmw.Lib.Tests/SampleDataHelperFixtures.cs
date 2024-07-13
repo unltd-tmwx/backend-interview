36c4bc35922f08fdae4213960f3c3db42b127c0a
Anthony Moloney <anthony.moloney@gmail.com>
2024-07-13 12:28:18 +0100

    Add customer dto for json

--- src/Tmw.Lib.Tests/SampleDataHelperFixtures.cs
+++ src/Tmw.Lib.Tests/SampleDataHelperFixtures.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Tmw.Lib.Data.sample_data;
 
 namespace Tmw.Lib.Tests;
@@ -9,5 +10,23 @@ public class SampleDataHelperFixtures
     {
         var json = SampleDataHelper.ReadEmbeddedCustomerData();
         Assert.NotNull(json);
+    }
+
+    [Fact]
+    public void CanDeserializeSampleDataItem()
+    {
+        var sampleJson = """{"id":"541d25c9-9500-4265-8967-240f44ecf723","name":"Samir Pacocha","age":46,"acceptedOffers":49,"canceledOffers":92,"averageReplyTime":2598,"location":{"latitude":"46.7110","longitude":"-63.1150"}}"""; ;
+
+        // act
+        var customerDto = JsonSerializer.Deserialize<CustomerDto>(sampleJson);
+
+        Assert.NotNull(customerDto);
+        Assert.Equal("Samir Pacocha", customerDto.Name);
+        Assert.Equal(46, customerDto.Age);
+        Assert.Equal(49, customerDto.AcceptedOffers);
+        Assert.Equal(92, customerDto.CancelledOffers);
+        Assert.Equal(2598, customerDto.AverageReplyTime);
+        Assert.Equal("46.7110", customerDto.Location.Latitude);
+        Assert.Equal("-63.1150", customerDto.Location.Longitude);
     }
 }
---
+++ src/Tmw.Lib/Data/sample-data/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using static Tmw.Lib.Data.sample_data.CustomerDto;
+
+namespace Tmw.Lib.Data.sample_data;
+
+internal record CustomerDto(
+    [property: JsonPropertyName("id")] Guid Id,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("age")] int Age,
+    [property: JsonPropertyName("acceptedOffers")] int AcceptedOffers,
+    [property: JsonPropertyName("canceledOffers")] int CancelledOffers,
+    [property: JsonPropertyName("averageReplyTime")] int AverageReplyTime,
+    [property: JsonPropertyName("location")] CoordinatesDto Location
+)
+{
+    internal record CoordinatesDto(
+        [property: JsonPropertyName("latitude")] string Latitude,
+        [property: JsonPropertyName("longitude")] string Longitude);
+};
