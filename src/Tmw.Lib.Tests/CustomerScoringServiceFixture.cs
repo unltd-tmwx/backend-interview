7c7e19380c8d55f7b6b304661913c8739ba5713c
Anthony Moloney <anthony.moloney@gmail.com>
2024-07-13 13:39:26 +0100

    Add normalization calculation to CustomerScoringService

---
+++ src/Tmw.Lib.Tests/CustomerScoringServiceFixture.cs
@@ -0,0 +1,63 @@
+﻿using Tmw.Lib.Data;
+using Tmw.Lib.Model;
+using Tmw.Lib.Services;
+
+namespace Tmw.Lib.Tests;
+
+public class CustomerScoringServiceFixture
+{
+    [Fact]
+    public void CanInitialiseCustomerScoringDto()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+
+        // act
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+
+        Assert.Equal(customers.Length, scoringDtos.Length);
+        Assert.All(scoringDtos, s => Assert.Equal(0, s.AgeScore));
+        Assert.All(scoringDtos, s => Assert.True(s.DistanceFromBase > 0));
+    }
+
+    [Fact]
+    public void CanGetAgeCategoryRangeMinMax()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+
+        // act
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax( scoringDtos.Select(s => (double) s.Customer.Age).ToArray());
+
+        Assert.Equal(21, rangeMin);
+        Assert.Equal(90, rangeMax);
+    }
+
+    [Fact]
+    public void CanNormaliseAgeCategoryRange()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax( scoringDtos.Select(s => (double) s.Customer.Age).ToArray());
+        var customer = scoringDtos.First();
+
+        var normalised = CustomerScoringService.NormaliseToDefinedRange(customer.Customer.Age, rangeMin, rangeMax);
+
+        Assert.True(normalised >= 1 && normalised <= 10);
+    }
+
+    [Fact]
+    public void CanNormaliseAllAgeCategoryRange()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax(scoringDtos.Select(s => (double)s.Customer.Age).ToArray());
+        var customerAgeRange = scoringDtos.Select(s => (double)s.Customer.Age).ToArray();
+
+        // act
+        var normalisedAgeSet = customerAgeRange.Select(age => CustomerScoringService.NormaliseToDefinedRange(age, rangeMin, rangeMax)).ToArray();
+
+        Assert.All(normalisedAgeSet, age => Assert.True(age >= 0.0 && age <= 10.0));
+
+    }
+}
---
+++ src/Tmw.Lib/Services/CustomerScoringService.cs
@@ -0,0 +1,32 @@
+﻿using Tmw.Lib.Model;
+
+namespace Tmw.Lib.Services;
+
+public class CustomerScoringService : ICustomerScoringService
+{
+    public Customer[] GetCustomersByScore(Coordinates baseCoordinates)
+    {
+        throw new NotImplementedException();
+    }
+
+    static internal CustomerScoring[] Initialise(Customer[] customers, Coordinates baseCoordinates)
+    {
+        return customers.Select(c => new CustomerScoring(
+            c,
+            0,
+            c.Location.DistanceTo(baseCoordinates),
+            0,
+            0,
+            0,
+            0
+        )).ToArray();
+    }
+
+    static internal (double min, double max) GetRangeMinMax(double[] range) => (range.Min(), range.Max());
+
+    static internal double NormaliseToDefinedRange(double val, double min, double max, bool invert = false)
+    {
+        var score = 1 + (val - min) * 9 / (max - min);
+        return invert ? 10 - score : score;
+    }
+}
