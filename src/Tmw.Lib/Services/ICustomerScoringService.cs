df8abc5df4ddc4281479b308e2aee9617968f1fb
Anthony Moloney <anthony.moloney@gmail.com>
2024-07-13 12:53:07 +0100

    Add customer scoring service contract

---
+++ src/Tmw.Lib/Services/ICustomerScoringService.cs
@@ -0,0 +1,8 @@
+﻿using Tmw.Lib.Model;
+
+namespace Tmw.Lib.Services;
+
+public interface ICustomerScoringService
+{
+    Customer[] GetScoredCustomers(Coordinates baseCoordinates);
+}
---
+++ src/Tmw.Lib/Services/WeightingCategories.cs
@@ -0,0 +1,11 @@
+﻿namespace Tmw.Lib.Services;
+
+// This could be a configuration file, but for the sake of simplicity, I'm using a static class.
+internal class WeightingCategories
+{
+    public const double DemographicAge = 0.1;
+    public const double DemographicDistance = 0.1;
+    public const double BehaviourAcceptedOffers = 0.3;
+    public const double BehaviourCancelledOffers = 0.3;
+    public const double BehaviourReplyTime = 0.2;
+}
