651d919273d7936cebb6956dcbf8fd3d9ef461d4
Anthony Moloney <anthony.moloney@gmail.com>
2024-07-13 12:48:46 +0100

    validate coordinate distance formula nicked from internet

--- src/Tmw.Lib/Model/Customer.cs
+++ src/Tmw.Lib/Model/Customer.cs
@@ -10,4 +10,27 @@ public record Customer(
     Coordinates Location
     );
 
-public record Coordinates(double Latitude, double Longitude);
+public record Coordinates(double Latitude, double Longitude)
+{
+    // nicked from internet
+    public double DistanceTo(Coordinates other)
+    {
+        double earthRadiusKm = 6371;
+
+        double dLat = DegreesToRadians(other.Latitude - Latitude);
+        double dLon = DegreesToRadians(other.Longitude - Longitude);
+
+        double lat1 = DegreesToRadians(Latitude);
+        double lat2 = DegreesToRadians(other.Latitude);
+
+        double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        return earthRadiusKm * c;
+    }
+
+    private double DegreesToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180;
+    }
+}
