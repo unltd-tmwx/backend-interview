6f5f5534ccc08cc6cd33f4596ca665c21a238a7e
Anthony Moloney <anthony.moloney@gmail.com>
2024-07-13 13:49:38 +0100

    Validate normalisation calculations

--- src/Tmw.Lib.Tests/CustomerScoringServiceFixture.cs
+++ src/Tmw.Lib.Tests/CustomerScoringServiceFixture.cs
@@ -58,7 +58,57 @@ public class CustomerScoringServiceFixture
         var normalisedAgeSet = customerAgeRange.Select(age => CustomerScoringService.NormaliseToDefinedRange(age, rangeMin, rangeMax)).ToArray();
 
         Assert.All(normalisedAgeSet, age => Assert.True(age >= 0.0 && age <= 10.0));
-        Assert.Equal(normalisedAgeSet.Min(), 1.0);
-        Assert.Equal(normalisedAgeSet.Max(), 10.0);
+        Assert.Equal( 1.0, normalisedAgeSet.Min());
+        Assert.Equal( 10.0, normalisedAgeSet.Max());
     }
+
+    [Fact]
+    public void CanNormaliseAllDistanceCategoryRangeAndInvert()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax(scoringDtos.Select(s => s.DistanceFromBase).ToArray());
+        var customerDistanceRange = scoringDtos.Select(s => s.DistanceFromBase).ToArray();
+
+        // act
+        var normalisedDistanceSet = customerDistanceRange.Select(distance => CustomerScoringService.NormaliseToDefinedRange(distance, rangeMin, rangeMax, true)).ToArray();
+
+        Assert.All(normalisedDistanceSet, distance => Assert.True(distance >= 0.0 && distance <= 10.0));
+        Assert.Equal(0, normalisedDistanceSet.Min());
+        Assert.Equal( 9, normalisedDistanceSet.Max());
+    }
+
+    [Fact]
+    public void CanNormaliseAllAcceptedOffersCategoryRange()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax(scoringDtos.Select(s => (double) s.Customer.AcceptedOffers).ToArray());
+        var customerAcceptedOffersRange = scoringDtos.Select(s => s.Customer.AcceptedOffers).ToArray();
+
+        // act
+        var normalisedAcceptedOffersSet = customerAcceptedOffersRange.Select(acceptedOffers => CustomerScoringService.NormaliseToDefinedRange(acceptedOffers, rangeMin, rangeMax)).ToArray();
+
+        Assert.All(normalisedAcceptedOffersSet, acceptedOffers => Assert.True(acceptedOffers >= 0.0 && acceptedOffers <= 10.0));
+        Assert.Equal( 1.0, normalisedAcceptedOffersSet.Min());
+        Assert.Equal( 10.0, normalisedAcceptedOffersSet.Max());
+    }
+
+    [Fact]
+    public void CanNormaliseAllCancelledOffersCategoryRangeAndInvert()
+    {
+        var customers = new CustomerSampleRepository().GetAllCustomers();
+        var scoringDtos = CustomerScoringService.Initialise(customers, new Coordinates(0.0, 0.0));
+        var (rangeMin, rangeMax) = CustomerScoringService.GetRangeMinMax(scoringDtos.Select(s => (double) s.Customer.CancelledOffers).ToArray());
+        var customerCancelledOffersRange = scoringDtos.Select(s => s.Customer.CancelledOffers).ToArray();
+
+        // act
+        var normalisedCancelledOffersSet = customerCancelledOffersRange.Select(cancelledOffers => CustomerScoringService.NormaliseToDefinedRange(cancelledOffers, rangeMin, rangeMax, true)).ToArray();
+
+        Assert.All(normalisedCancelledOffersSet, cancelledOffers => Assert.True(cancelledOffers >= 0.0 && cancelledOffers <= 10.0));
+        Assert.Equal( 0, normalisedCancelledOffersSet.Min());
+        Assert.Equal( 9, normalisedCancelledOffersSet.Max());
+    }
+
+    
 }
